c042486db7cbfe65cbc7fa29fed8450d649c2777
MaxWhite2024 <mjwhite@scu.edu>
2025-01-18 15:24:47 -0500

    vertical camera movement working

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -7,36 +7,34 @@ using UnityEngine.InputSystem;
 public class Player_Movement : MonoBehaviour
 {
     //TO DO: 
-    // - rename variables to camelcase
-    // - rename horizontal_Pivot to horizontalPivot
     // - add verticalPivot gameobject
 
     [Header("Player Movement Variables")]
-    [SerializeField] private Rigidbody player_Rigidbody;
-    [SerializeField] private Transform player_Horizontal_Transform;
-    [SerializeField] private float move_Force = 0f;
-    private bool is_Player_Horizontal_Moving = false;
-    private Vector3 move_Dir = Vector3.zero;
+    [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private float moveForce = 0f;
+    private bool isPlayerHorizontalMoving = false;
+    private Vector3 moveDir = Vector3.zero;
 
     [Header("Camera Movement Variables")]
-    [SerializeField] private Transform horizontal_Pivot;
-    [SerializeField] private float camera_Movespeed = 0f;
-    private bool is_Camera_Horizontal_Turning = false;
-    private Vector3 camera_Turn_Dir = Vector3.zero;
+    [SerializeField] private Transform horizontalCameraPivot;
+    [SerializeField] private Transform verticalCameraPivot;
+    [SerializeField] private float cameraMovespeed = 0f;
+    private bool isCameraHorizontalTurning, isCameraVerticalTurning = false;
+    private Vector3 cameraTurnDir = Vector3.zero;
 
     private void FixedUpdate()
     {
-        //Debug.Log(horizontal_Pivot.forward);
+        //Debug.Log(horizontalPivot.forward);
         //if player is inputting a player movement input,...
-        if (is_Player_Horizontal_Moving)
+        if (isPlayerHorizontalMoving)
         {
             //get player input
-            float playerVerticalInput = move_Dir.z;
-            float playerHorizontalInput = move_Dir.x;
+            float playerVerticalInput = moveDir.z;
+            float playerHorizontalInput = moveDir.x;
 
             //get horizontal pivot direction
-            Vector3 forward = horizontal_Pivot.forward;
-            Vector3 right = horizontal_Pivot.right;
+            Vector3 forward = horizontalCameraPivot.forward;
+            Vector3 right = horizontalCameraPivot.right;
 
             //remove upwards components from forward and right
             forward.y = 0f;
@@ -53,15 +51,22 @@ public class Player_Movement : MonoBehaviour
             //calculate pivotRelativeMovement
             Vector3 pivotRelativeMovement = forwardRelativeHorizontalInput + rightRelativeHorizontalInput;
 
-            //apply move_Force force to the player in the horizontal direction the camera is facing
-            player_Rigidbody.AddForce(pivotRelativeMovement * move_Force, ForceMode.Force);
+            //apply moveForce force to the player in the horizontal direction the camera is facing
+            playerRigidbody.AddForce(pivotRelativeMovement * moveForce, ForceMode.Force);
         }
 
+        //Debug.Log(cameraTurnDir);
         //is player inputting a camera turning input,...
-        if(is_Camera_Horizontal_Turning)
+        if(isCameraHorizontalTurning)
         {
-            //rotate the horizontal camera pivot point by camera_Turn_Dir.x
-            horizontal_Pivot.Rotate(0f, camera_Turn_Dir.x, 0f);
+            //rotate the horizontal camera pivot point by cameraTurnDir.x
+            horizontalCameraPivot.Rotate(0f, cameraTurnDir.x * cameraMovespeed, 0f);
+        }
+
+        if(isCameraVerticalTurning)
+        {
+            //rotate the vertical camera pivot point by cameraTurnDir.y clamped between -90 and 90
+            verticalCameraPivot.Rotate(cameraTurnDir.y * cameraMovespeed, 0f, 0f);
         }
     }
 
@@ -70,31 +75,45 @@ public class Player_Movement : MonoBehaviour
         //if player started moving the player horizontally,...
         if(value.Get<Vector2>() != Vector2.zero)
         {
-            is_Player_Horizontal_Moving = true;
-            move_Dir.z = value.Get<Vector2>().y;
-            move_Dir.x = value.Get<Vector2>().x;
+            isPlayerHorizontalMoving = true;
+            moveDir.z = value.Get<Vector2>().y;
+            moveDir.x = value.Get<Vector2>().x;
         }
         //else player stopped moving the player horizontally,...
         else
         {
-            is_Player_Horizontal_Moving = false;
-            move_Dir = Vector2.zero;
+            isPlayerHorizontalMoving = false;
+            moveDir = Vector2.zero;
         }
     }
 
     void OnCameraMove(InputValue value)
     {
+        Debug.Log(value.Get<Vector2>());
         //if player started turning the camera horizontally,...
         if(value.Get<Vector2>().x != 0f)
         {
-            is_Camera_Horizontal_Turning = true;
-            camera_Turn_Dir.x = value.Get<Vector2>().x;
+            isCameraHorizontalTurning = true;
+            cameraTurnDir.x = value.Get<Vector2>().x;
         }
         //else player stopped turning the camera horizontally,...
         else
         {
-            is_Camera_Horizontal_Turning = false;
-            camera_Turn_Dir.x = 0f;
+            isCameraHorizontalTurning = false;
+            cameraTurnDir.x = 0f;
+        }
+
+        //if player started turning the camera vertically,...
+        if (value.Get<Vector2>().y != 0f)
+        {
+            isCameraVerticalTurning = true;
+            cameraTurnDir.y = value.Get<Vector2>().y;
+        }
+        //else player stopped turning the camera vertically,...
+        else
+        {
+            isCameraVerticalTurning = false;
+            cameraTurnDir.y = 0f;
         }
     }
 }
