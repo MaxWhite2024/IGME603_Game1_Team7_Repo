05aac39bd296303bf123a217f268a807bb1a3841
Matt Izzo <46375091+Mattyiz@users.noreply.github.com>
2025-01-19 12:33:32 -0500

    Push so I can pull from main

--- Assets/Scripts/PlayerRatCollection.cs
+++ Assets/Scripts/PlayerRatCollection.cs
@@ -27,10 +27,12 @@ public class PlayerRatCollection : MonoBehaviour
         {
             //Debug.Log("Rat is here");
             ratCount += rat.ratCount;
-            Destroy(rat.gameObject);
+            collider.transform.parent = transform;
 
-            scale = new Vector3(scale.x + ratScaleAmount.x, scale.y + ratScaleAmount.y, scale.z + ratScaleAmount.z);
-            transform.localScale = scale;
+            //Destroy(rat.gameObject);
+
+            //scale = new Vector3(scale.x + ratScaleAmount.x, scale.y + ratScaleAmount.y, scale.z + ratScaleAmount.z);
+            //transform.localScale = scale;
             return;
         }
 
@@ -56,11 +58,29 @@ public class PlayerRatCollection : MonoBehaviour
         BouncyThings bouncy = collider.GetComponent<BouncyThings>();
         if(bouncy != null)
         {
+            gameObject.GetComponent<Rigidbody>().useGravity = false; //Disables gravity so you don't fall down
+
+            if (gameObject.GetComponent<Rigidbody>().velocity.y > 0)
+            {
+                return;
+            }
+
+            //This is part of the super cursed don't fall through the bounce code. Sets velocity to 0 when it's too low so you don't forever bounce
+            if (gameObject.GetComponent<Rigidbody>().velocity.y >= (0 - 1.5f) && gameObject.GetComponent<Rigidbody>().velocity.y <= (0 + 1.5f))
+            {
+                Vector3 zeroVelocity = gameObject.GetComponent<Rigidbody>().velocity;
+                zeroVelocity.y = 0;
+                gameObject.GetComponent<Rigidbody>().velocity = zeroVelocity;
+                return;
+            }
+
+            Debug.Log(gameObject.GetComponent<Rigidbody>().velocity);
             Vector3 newVelocity = gameObject.GetComponent<Rigidbody>().velocity;
             newVelocity.y = newVelocity.y * bouncy.bounceDampener;
-            Debug.Log(newVelocity.y);
+            //Debug.Log(newVelocity.y);
 
             gameObject.GetComponent<Rigidbody>().velocity = newVelocity;
+            Debug.Log(gameObject.GetComponent<Rigidbody>().velocity);
         }
 
     }
@@ -73,6 +93,12 @@ public class PlayerRatCollection : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         CollisionLogic(other);
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        gameObject.GetComponent<Rigidbody>().useGravity = true; //Enables gravity when you exit bouncy thing
 
     }
 }
