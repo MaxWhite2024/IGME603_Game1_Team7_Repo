8fd744297bef9797fc7057323cd81db078488b86
Luke Justice <justicelu97@gmail.com>
2025-01-23 15:52:26 -0500

    adding rat count ui to auto find player

--- Assets/Scripts/UI/RatCountUI.cs
+++ Assets/Scripts/UI/RatCountUI.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -6,7 +7,15 @@ public class RatCountUI : MonoBehaviour
     [SerializeField] private PlayerRatCollection player;
     [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private int requirement = 20;
-    
+
+    private void Start()
+    {
+        if (!player)
+        {
+            player = FindObjectOfType<PlayerRatCollection>();
+        }
+    }
+
     private void Update()
     {
         text.text = $"{player.ratCount}/{requirement}";
